a3c3f857b6a5cbc7e649f4333022e849991b05cf
Sathish Krishnamurthy <sathish.krishnamurthy@oracle.com>
2021-05-23 18:58:15 +0530

    update modal attributes

--- src/main/java/com/multitenant/saml/controller/HomeController.java
+++ src/main/java/com/multitenant/saml/controller/HomeController.java
@@ -1,11 +1,15 @@
 package com.multitenant.saml.controller;
 
+import com.multitenant.saml.Product;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -29,7 +33,23 @@ public class HomeController {
     public String home(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("username", authentication.getPrincipal());
+        model.addAttribute("products", getProducts());
         return "home";
+    }
+
+
+    public List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
+        for (int i=1;i<=10;i++) {
+            Product product = new Product();
+            product.setId(""+i);
+            product.setCategory("Category"+i);
+            product.setName("Name"+i);
+            product.setPrice(""+i);
+            products.add(product);
+        }
+
+        return products;
     }
 
 }
